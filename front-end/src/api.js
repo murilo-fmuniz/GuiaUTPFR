3926ea1f5eab0c80b42914c04c25271237c6fd8e
Murilo Fontana Muniz <muri.fmuniz@gmail.com>
2025-12-01 12:17:06 -0300

    feat: update API_BASE to use VITE_API_URL environment variable

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,6 +1,6 @@
 // API utility for authenticated requests
 
-const API_BASE = 'http://localhost:8000';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 // Get token from localStorage
 function getToken() {
